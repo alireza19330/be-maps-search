05f2368ba17c816807d6a65d37a452e36cc1c12b
alireza19330 <alireza19330@gmail.com>
2024-03-09 01:00:31 +1100

    make api to return the interface type

--- src/maps-api.ts
+++ src/maps-api.ts
@@ -1,16 +1,16 @@
-import axios from 'axios'
+import axios from "axios";
+import { TomtomSearchResult } from "./tomtomSearchResult";
 
 // https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
 export async function getPlaceAutocomplete(key: string, address: string) {
-    const autocomplete = await axios.get(`https://api.tomtom.com/search/2/search/${address}.json'`, {
-        params: {
-            key,
-            limit: 100,
-        }
-    });
-    return autocomplete.data.results.map((result) => {
-        return {
-            placeId: result.id,
-        }
-    })
+  const autocomplete = await axios.get(
+    `https://api.tomtom.com/search/2/search/${address}.json'`,
+    {
+      params: {
+        key,
+        limit: 100,
+      },
+    }
+  );
+  return autocomplete.data.results as TomtomSearchResult[];
 }
