d1fc49a453bd124fed1790818fb37c175fb195a8
alireza19330 <alireza19330@gmail.com>
2024-03-09 01:01:01 +1100

    convert api response to internal type

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
-import { getPlaceAutocomplete } from './maps-api'
+import { getPlaceAutocomplete } from "./maps-api";
+import { SearchResult } from "./searchResult";
+import { TomtomSearchResult } from "./tomtomSearchResult";
 
 export async function getAutoCompleteDetails(address: any): Promise<any> {
-    const apiKey = process.env.TOMTOM_API_KEY;
-    // get autocomplete results
-    const res = getPlaceAutocomplete(process.env.TOMTOM_API_KEY, address).then(async (autocompleteResults) => {
-        const res = []
-        return res
+  const apiKey = process.env.TOMTOM_API_KEY;
+  // get autocomplete results
+  const res = getPlaceAutocomplete(apiKey, address)
+    .then(async (autocompleteResults: TomtomSearchResult[]) => {
+      return autocompleteResults.map((result) => convert(result));
     })
-    // loop over and get details and map results
-    return res
+    .catch((error: Error) => {
+      console.error(error.message);
+    });
+  return res;
+}
+
+function convert(source: TomtomSearchResult): SearchResult {
+  return {
+    type: source.type,
+    placeId: source.id,
+    address: {
+      streetNumber: source.address.streetNumber,
+      streetName: source.address.streetName,
+      municipalitySubdivision: source.address.municipalitySubdivision,
+      municipality: source.address.municipality,
+      countrySubdivision: source.address.countrySubdivision,
+      postalCode: source.address.postalCode,
+      country: source.address.country,
+      countryCode: source.address.countryCode,
+      freeformAddress: source.address.freeformAddress,
+    },
+  };
 }
